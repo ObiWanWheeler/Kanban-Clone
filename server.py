c71ab378372e41149b1a475438cfca0adc08b529
ObiWanWheeler <charliestinhorto@gmail.com>
2021-02-11 17:43:59 +0000

    add task editing

--- server.py
+++ server.py
@@ -49,7 +49,7 @@ class ListColumn(db.Model):
 class ListItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     parent_list_id = db.Column(db.Integer, db.ForeignKey("listcolumn.id"), nullable=False)
-    task_name = db.Column(db.String(200), nullable=False, unique=True)
+    task_name = db.Column(db.String(200), nullable=False)
     task_description = db.Column(db.String(200), nullable=False)
 
 
@@ -94,6 +94,7 @@ def add_list():
 def list(list_id):
     requested_list = db.session.query(TODOList).get(list_id)
     if requested_list is not None:
+        print('here')
         form = ListItemForm()
         return render_template("list.html", form=form, list=requested_list)
     else:
@@ -106,14 +107,12 @@ def add_item(list_id):
     list = db.session.query(TODOList).get(list_id)
     if form.validate_on_submit():
         column_to_append_to = None
-        print(form.column.data)
         for list in list.sub_lists:
             print(list.list_type)
             if list.list_type == form.column.data:
                 column_to_append_to = list
                 break
         if column_to_append_to is not None:
-            print("here")
             new_item = ListItem(parent_list_id=column_to_append_to.id,
                                 task_description=form.task_description.data, task_name=form.task_name.data)
             db.session.add(new_item)
@@ -130,9 +129,34 @@ def delete_item(item_id, list_id):
     return redirect(url_for("list", list_id=list_id))
 
 
-@app.route("/edit-item/<int:list_id>/<int:item_id>")
+@app.route("/edit-item/<int:list_id>/<int:item_id>", methods=["GET", "POST"])
 def edit_item(item_id, list_id):
     item_to_edit = db.session.query(ListItem).get(item_id)
+    list = db.session.query(TODOList).get(list_id)
+
+    if list is None:
+        return redirect(url_for("lists"))
+    if item_to_edit is None:
+        return redirect(url_for("list", list_id=list.id))
+
+    form = ListItemForm(task_name=item_to_edit.task_name, task_description=item_to_edit.task_description)
+
+    if form.validate_on_submit():
+        list = db.session.query(TODOList).get(list_id)
+        item_to_edit.task_name = form.task_name.data
+        item_to_edit.task_description = form.task_description.data
+        list.last_edited = dt.utcnow()
+        column_to_append_to = None
+        for list in list.sub_lists:
+            if list.list_type == form.column.data:
+                column_to_append_to = list
+                break
+        if column_to_append_to is not None:
+            item_to_edit.parent_list_id = column_to_append_to.id
+        db.session.commit()
+        return redirect(url_for("list", list_id=list_id))
+    else:
+        return render_template("edit.html", form=form, list=list)
 
 
 if __name__ == '__main__':
